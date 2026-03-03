67ac1bf3487ba3d111a968a10c0086e899128cad
Bourny90 <Kierenbourn@outlook.com>
2026-03-03 20:06:25 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,10 +3,17 @@ window.addEventListener("load", () => {
   document.getElementById("loader").style.display = "none";
 });
 
-// Sidebar toggle
+// Sidebar
 const burger = document.getElementById("burger");
 const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
 
 burger.addEventListener("click", () => {
   sidebar.classList.toggle("open");
+  overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("open");
+  overlay.classList.remove("active");
 });
