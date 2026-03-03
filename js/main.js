fbb929f1f6ebf8c62b6dd5486bba7db1c15fb493
Bourny90 <Kierenbourn@outlook.com>
2026-03-03 19:48:44 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-.
+// BURGER MENU
+const burger = document.getElementById('burger');
+const sidebar = document.getElementById('sidebar');
+const links = document.querySelectorAll('.nav-link');
+const indicator = document.getElementById('indicator');
+
+burger.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+});
+
+// Slider Indicator
+links.forEach(link => {
+  link.addEventListener('click', function () {
+    indicator.style.top = this.offsetTop + "px";
+  });
+});
