9d64d7c54e5f70b083c0958634e73e4643e40b01
Bourny90 <Kierenbourn@outlook.com>
2026-04-23 18:40:25 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// app.js
-
-import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
-
-// test login
-signInWithEmailAndPassword(auth, "test@test.com", "password123")
-  .then(user => console.log("Logged in:", user.user))
-  .catch(err => console.error(err.message));
